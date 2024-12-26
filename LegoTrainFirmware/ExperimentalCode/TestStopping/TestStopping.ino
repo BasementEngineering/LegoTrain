0d301c1743344a532a35746d1d1e145ed3097a47
TransitSenseDev <j.kettler@transitsense.de>
2024-12-27 00:27:40 +0100

    Added
    Cleaned the state machine in the testStoppinng Code

--- LegoTrainFirmware/ExperimentalCode/TestStopping/TestStopping.ino
+++ LegoTrainFirmware/ExperimentalCode/TestStopping/TestStopping.ino
@@ -29,161 +29,242 @@ SpeedMonitor speedMonitor;
 unsigned long lastMotorSpeedUpdate = 0;
 unsigned long lastEventUpdate = 0;
 
-#define STOP_DURATION 3000
+unsigned long lastStopTime = 0;
+int longestTimeBetweenStops = 0;
 
-enum modes {
+#define STOP_DURATION 5000
+
+#define BRAKE_DURATION 1000
+
+enum modes
+{
     AT_START_POSITION,
     GOING_FORWARD,
-    STOPPED_FW,
-    STOPPED_RW,
+    BRAKING,
+    STOPPED,
     CORRECTING,
     GOING_BACKWARD
 };
 
+enum directions
+{
+    FORWARD,
+    BACKWARD,
+    NONE
+};
+
+int currentDirection = NONE;
+
 int currentState = AT_START_POSITION;
 #define TRAVEL_SPEED 50
 
-void handleStopInterrupt(){
-  detachInterrupt(MAGNET_BOTTOM_PIN); 
+bool middleCleared = false;
+int extraStops = 0;
+
+void handleStopInterrupt()
+{
+    detachInterrupt(MAGNET_BOTTOM_PIN);
     controlLoop.reset();
     newSetpoint = 0;
-    motor.brake(); //Only works when moving forwards
-    //motor.setSpeed(0);
-    if(currentState == GOING_FORWARD){
-        currentState = STOPPED_FW;
-    }
-    else if(currentState == CORRECTING){
-        currentState = STOPPED_RW;
-    }
+    motor.brake(); // Only works when moving forwards
+    // motor.setSpeed(0);
+    
 
     digitalWrite(INTERRUPT_CHECK_PIN, !digitalRead(INTERRUPT_CHECK_PIN));
     lastEventUpdate = millis();
+    middleCleared = true;
+    currentState = BRAKING;
 }
 
-void setStopIntterrupt(){
+void setStopIntterrupt()
+{
     attachInterrupt(MAGNET_BOTTOM_PIN, handleStopInterrupt, FALLING);
 }
 
-bool middleCleared = false;
+void checkInterStopTimer()
+{
+    if (shotCount > 1)
+    {
+        int timeDelta = millis() - lastStopTime - STOP_DURATION;
+        if (timeDelta > longestTimeBetweenStops)
+        {
+            longestTimeBetweenStops = timeDelta;
+            speedMonitor.setDistance((float)timeDelta * 0.001);
+        }
 
-//Have to add timeout, while driving forward. Start it after the first shot spot was reached. 
-//And if no more is reached after a couple of mm or seconds just go to target.
-void runTest(){
+        Serial.println();
+        Serial.print("Longest Time Between Stops: ");
+        Serial.println(longestTimeBetweenStops);
+        Serial.print("Time Delta: ");
+        Serial.println(timeDelta);
+        Serial.println();
+    }
+}
+
+void printMetrics()
+{
+    speedMonitor.setTotalDistance(avgDistance);
+
+    Serial.println("---------------------");
+    Serial.print("Round: ");
+    Serial.println(roundCounter);
+    Serial.print("Misses: ");
+    Serial.println(misses);
+    Serial.print("Incomplete Runs: ");
+    Serial.println(incompleteRuns);
+    Serial.print("Avg Distance: ");
+    Serial.println(avgDistance);
+    Serial.println("---------------------");
+}
+
+void onEndReached()
+{
+    if (shotCount < 6)
+    {
+        incompleteRuns++;
+        Serial.println("Incomplete Run");
+        shotCount = 0;
+    }
+    endDistance = speedometer.getDistance();
+
+    int distance = endDistance - startDistance;
+    if (avgDistance == -1)
+    {
+        avgDistance = distance;
+    }
+    else
+    {
+        avgDistance = (avgDistance + distance) / 2;
+    }
+    
+    Serial.print("Distance: ");
+    Serial.println(distance);
+
+    lastStopTime = 0;
+    roundCounter++;
+    printMetrics();
+}
+
+void switchState(int newState){
+    String stateString[] = {"AT_START_POSITION", "GOING_FORWARD", "BRAKING", "STOPPED", "CORRECTING", "GOING_BACKWARD"};
+
+    Serial.println("Switching State");	
+    Serial.print("From: ");
+    Serial.println(stateString[currentState]);
+    Serial.print("To: ");
+    Serial.println(stateString[newState]);
+    
+    currentState = newState;
+    lastEventUpdate = millis();
+}
+
+void creepToStop(int speed)
+{
+    if(speed > 0){
+        currentDirection = FORWARD;
+    }
+    else{
+        currentDirection = BACKWARD;
+    }
+    newSetpoint = speed;
+    lastStopTime = 0;
+    setStopIntterrupt();
+}
+// Have to add timeout, while driving forward. Start it after the first shot spot was reached.
+// And if no more is reached after a couple of mm or seconds just go to target.
+void runTest()
+{
     switch (currentState)
     {
     case AT_START_POSITION:
-        if(millis() - lastEventUpdate > STOP_DURATION){
-            lastEventUpdate = millis();
-            newSetpoint = TRAVEL_SPEED;
-            setStopIntterrupt();
+        if (millis() - lastEventUpdate > STOP_DURATION)
+        {
             Serial.println("Starting at start position");
             Serial.println("Going forward");
             shotCount = 0;
-            currentState = GOING_FORWARD;
+
             startDistance = speedometer.getDistance();
+            
+            creepToStop(TRAVEL_SPEED);
+            switchState(GOING_FORWARD);
         }
         break;
-    case STOPPED_FW:
-        if(millis() - lastEventUpdate > 1000){
-            if(!magnetSensors.bottomMagnetPresent()){
-                newSetpoint = -TRAVEL_SPEED;
-                setStopIntterrupt();
-                Serial.println("Stopped FW Correcting");
+    case BRAKING:
+        if (millis() - lastEventUpdate > BRAKE_DURATION)
+        {
+            if (!magnetSensors.bottomMagnetPresent())
+            {
                 Serial.println("Missed Magnet");
                 misses++;
-                Serial.println("Correcting");
-                currentState = CORRECTING;
-                lastEventUpdate = millis();
+
+                if(currentDirection == FORWARD){
+                    creepToStop(-TRAVEL_SPEED);
+                    switchState(CORRECTING);
+                }
+                else{
+                    creepToStop(TRAVEL_SPEED);
+                    switchState(GOING_FORWARD);
+                }   
+            }
+            else
+            {
+                Serial.println("Stopped On Point");
+                currentDirection = NONE;
+                switchState(STOPPED);
             }
         }
-        if(millis() - lastEventUpdate > STOP_DURATION){
-            lastEventUpdate = millis();
-            newSetpoint = TRAVEL_SPEED;
-            setStopIntterrupt();
-            Serial.println("Stopped On Point");
+        break;
+    case STOPPED:
+        if(lastStopTime != 0){
+            if(currentDirection == FORWARD){
+                checkInterStopTimer();
+                lastStopTime = 0;
+            }
+        }
+
+        if (millis() - lastEventUpdate > STOP_DURATION)
+        {
             shotCount++;
-            Serial.println("Going forward");
-            currentState = GOING_FORWARD;
-        }
-        break;
-    case STOPPED_RW:
-        if(millis() - lastEventUpdate > 1000){
-            if(!magnetSensors.bottomMagnetPresent()){
-                Serial.println("Stopped RW Correcting");
-                Serial.println("Missed Magnet");
-                misses++;
-                setStopIntterrupt();
-                newSetpoint = TRAVEL_SPEED;
-                currentState = GOING_FORWARD;
-                lastEventUpdate = millis();
-            }
-        }
-        if(millis() - lastEventUpdate > STOP_DURATION){
-            lastEventUpdate = millis();
-            setStopIntterrupt();
-            Serial.println("GOING FORWARD");
-            newSetpoint = TRAVEL_SPEED;
-            currentState = GOING_FORWARD;
+
+            lastStopTime = millis();
+
+            creepToStop(TRAVEL_SPEED);
+            switchState(GOING_FORWARD);
         }
         break;
     case CORRECTING:
         break;
     case GOING_FORWARD:
-      if(magnetSensors.bottomWasTriggered()){
+        if (magnetSensors.bottomWasTriggered())
+        {
             middleCleared = true;
         }
-        if(magnetSensors.sideWasTriggered() && middleCleared){
-            lastEventUpdate = millis();
+        if (magnetSensors.sideWasTriggered() && middleCleared)
+        {
             handleStopInterrupt();
-            newSetpoint = -TRAVEL_SPEED;
             Serial.println("End Reached");
+            
+            onEndReached();
 
-            if(shotCount < 6){
-                incompleteRuns++;
-                Serial.println("Incomplete Run");
-                shotCount = 0;
-            }
-            endDistance = speedometer.getDistance();
-            roundCounter++;
-            int distance = endDistance - startDistance;
-            if(avgDistance == -1){
-                avgDistance = distance;
-            }
-            else{
-                avgDistance = (avgDistance + distance) / 2;
-            }
-
-            speedMonitor.setTotalDistance(avgDistance);
-            speedMonitor.setDistance(distance);
-
-            Serial.println("---------------------");
-            Serial.print("Round: ");
-            Serial.println(roundCounter);
-            Serial.print("Misses: ");
-            Serial.println(misses);
-            Serial.print("Incomplete Runs: ");
-            Serial.println(incompleteRuns);
-            Serial.print("Distance: ");
-            Serial.println(distance);
-            Serial.print("Avg Distance: ");
-            Serial.println(avgDistance);
-            Serial.println("---------------------");
-
-            Serial.println("Going backward");
-            currentState = GOING_BACKWARD;
             middleCleared = false;
+            newSetpoint = -TRAVEL_SPEED;
+            currentDirection = BACKWARD;
+            switchState(GOING_BACKWARD);
         }
         break;
     case GOING_BACKWARD:
-    if(magnetSensors.bottomWasTriggered()){
+        if (magnetSensors.bottomWasTriggered())
+        {
             middleCleared = true;
         }
-        if(magnetSensors.sideWasTriggered() && middleCleared){
-            lastEventUpdate = millis();
+        if (magnetSensors.sideWasTriggered() && middleCleared)
+        {
             handleStopInterrupt();
-            Serial.println("Stopping");
-            currentState = AT_START_POSITION;
+            Serial.println("Reached Start Position");
             middleCleared = false;
+
+            currentDirection = NONE;
+            switchState(AT_START_POSITION);
         }
         break;
     default:
@@ -220,7 +301,7 @@ void loop()
     updateMotor();
 
     magnetSensors.update();
-  
+
     speedMonitor.setSpeed(speedometer.getSpeed());
 
     int testValues[4] = {roundCounter, incompleteRuns, misses, shotCount};
@@ -237,13 +318,14 @@ void updateMotor()
     if ((millis() - lastMotorSpeedUpdate) > 300)
     {
 
-      if(millis() - lastSpeedPrint > 1000){
+        if (millis() - lastSpeedPrint > 1000)
+        {
 
-        Serial.print("Speed: ");
-        Serial.println(speedometer.getSpeed());
-        lastSpeedPrint = millis();
-      }
-        
+            Serial.print("Speed: ");
+            Serial.println(speedometer.getSpeed());
+            lastSpeedPrint = millis();
+        }
+
         controlLoop.setInput(speedometer.getSpeed());
         controlLoop.setTarget(newSetpoint);
         motor.setSpeed(controlLoop.calculateOutput());
